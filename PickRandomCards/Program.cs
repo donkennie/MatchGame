05f29dcc1677a41d062287d49678e753d7666a5e
donkennie <ajeigbekehinde160@gmail.com>
2022-10-29 12:33:18 +0100

    create a new Console App project called PickRandomCards

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PickRandomCards;
+
+Console.WriteLine("Enter the number of cards to pick: ");
+
+string line = Console.ReadLine();
+
+if (int.TryParse(line, out int numberOfCards))
+{
+    foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+    {
+        Console.WriteLine(card);
+    }
+}
+else
+{
+    Console.WriteLine("Please enter a valid number.");
+}
